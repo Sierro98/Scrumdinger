1b3a9ee1d81a3fbc6549aa989a1908ff2b13bffb
Alejandro Sierro <121672722+Sierro98@users.noreply.github.com>
2023-09-30 20:48:52 +0200

    Added error handling

--- Scrumdinger/ScrumdingerApp.swift
+++ Scrumdinger/ScrumdingerApp.swift
@@ -9,6 +9,9 @@ struct ScrumdingerApp: App {
     @StateObject
     private var store = ScrumStore()
     
+    @State
+    private var errorWrapper: ErrorWrapper?
+    
     var body: some Scene {
         WindowGroup {
             ScrumsView(scrums: $store.scrums) {
@@ -16,17 +19,22 @@ struct ScrumdingerApp: App {
                     do {
                         try await store.save(scrums: store.scrums)
                     } catch {
-                        fatalError(error.localizedDescription)
+                        errorWrapper = ErrorWrapper(error: error, guidance: "Try again later")
                     }
                 }
             }
-                .task {
-                    do {
-                        try await store.load()
-                    } catch {
-                        fatalError(error.localizedDescription)
-                    }
+            .task {
+                do {
+                    try await store.load()
+                } catch {
+                    errorWrapper = ErrorWrapper(error: error, guidance: "Scrumdinger will load sample data and continue.")
                 }
+            }
+            .sheet(item: $errorWrapper) {
+                store.scrums = DailyScrum.sampleData
+            } content: { wrapper in
+                ErrorView(errorWrapper: wrapper)
+            }
         }
     }
 }
