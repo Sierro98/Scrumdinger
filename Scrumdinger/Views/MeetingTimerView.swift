fb55bbd1a571b047d619d2fdd8222c9f827be284
Alejandro Sierro <121672722+Sierro98@users.noreply.github.com>
2023-10-02 20:57:28 +0200

    progress in MeetingView, with bug

---
+++ Scrumdinger/Views/MeetingTimerView.swift
@@ -0,0 +1,56 @@
+//
+//  MeetingTimerView.swift
+//  Scrumdinger
+//
+//  Created by Alejandro Sierro Galán on 2/10/23.
+//
+
+import SwiftUI
+
+struct MeetingTimerView: View {
+    let speakers: [ScrumTimer.Speaker]
+    
+    let theme: Theme
+    
+    private var currIndex: Int
+    
+    private var currentSpeaker: String {
+        speakers.first(where: { !$0.isCompleted })?.name ?? "Someone"
+    }
+    
+    var body: some View {
+        Circle()
+            .strokeBorder(lineWidth: 24)
+            .overlay{
+                VStack {
+                    Text(currentSpeaker)
+                        .font(.title)
+                    Text(" is speaking")
+                }
+                .foregroundColor(theme.accentColor)
+                .accessibilityElement(children: .combine)
+            }
+            .overlay {
+                ForEach(speakers) { speaker in
+                    if speaker.isCompleted, let index = speakers.firstIndex(where: { $0.id == speaker.id }) {
+                        let _ = print(speakers)
+                        let _ = print(speaker.name, speaker.isCompleted)
+                        SpeakerArc(totalSpeakers: speakers.count, speakerIndex: index)
+                            .rotation(Angle(degrees: -90))
+                            .stroke(theme.mainColor, lineWidth: 12.0)
+                    }
+                }
+            }
+            .padding(.horizontal)
+    }
+}
+
+struct MeetingTimerView_Previews: PreviewProvider {
+    static var speakers: [ScrumTimer.Speaker] {
+        [ScrumTimer.Speaker(name: "Bill", isCompleted: true), ScrumTimer.Speaker(name: "Cathy", isCompleted: false)]
+    }
+    
+    static var previews: some View {
+        MeetingTimerView(speakers: speakers, theme: .yellow)
+    }
+}
